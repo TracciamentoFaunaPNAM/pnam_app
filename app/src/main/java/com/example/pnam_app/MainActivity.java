02cf9ae72717bd61a41b606f32d83eb83a9dbb6b
JeremyMinsenti <jerremymin2002@gmail.com>
2021-04-07 13:14:02 +0200

    Registration form, UI completed

--- app/src/main/java/com/example/pnam_app/MainActivity.java
+++ app/src/main/java/com/example/pnam_app/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.pnam_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.registration);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Registration.class);
+                startActivity(intent);
+            }
+        });
     }
 }
